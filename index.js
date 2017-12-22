94d0be8aa6603f2ce47e04b40836c215f47f713f
Filipe Fradique <filipe.fradique@b-i.com>
2017-12-22 01:56:50 +0000

    Added logger code

---
+++ index.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+const getLoggerParams = (params: any) => {
+  if (Array.isArray(params) && params.length) {
+    if (params[0] && typeof params[0] === 'string') {
+      const title = params[0]
+      if (title && params[1] && params[1].action) {
+        const action = params[1].action
+        const type = action.type || 'Undefined'
+        const error = params[1].error
+        return ({ title, type, action, error })
+      }
+    }
+  }
+  return ({ title: false, type: false, action: false, error: false })
+}
+
+export const logger = (...params: any) => {
+  const logger = console
+  const { title, type, action, error } = getLoggerParams(params)
+  if (title && type && action) {
+    logger.group(`%c ${title}%c ${type} 🦄 📚 🦄`, 'color: gray; font-weight: lighter;', 'font-weight: bold;')
+    logger.log('%c action    ', 'color: #03A9F4; font-weight: bold;', action)
+    if (error) {
+      logger.log('%c error     ', 'color: #F20404; font-weight: bold;', error)
+    }
+    logger.groupEnd()
+  } else {
+    logger.log(params)
+  }
+}
